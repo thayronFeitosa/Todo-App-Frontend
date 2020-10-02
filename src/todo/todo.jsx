b6aac9eb885e682cc9b30b1cfd90b010a64f7349
thayronFeitosa <thayronf3@gmail.com>
2020-10-02 15:43:05 -0300

    Usando o setState para manipular o valor do input

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import PageHeader from '../template/pageHeader'
-import todoForm from './todoForm';
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
@@ -14,18 +13,30 @@ export default class Todo  extends Component{
 
     constructor(props){
         super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
 
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+
     handleAdd(){
-        console.log(this);
+        console.log(this.state.description);
     }
 
     render(){
         return(
             <div>
               <PageHeader name="Tarefas" small="Cadastro" />
-              <TodoForm handleAdd={this.handleAdd}/>
+              <TodoForm 
+              description={this.state.description}
+              handleChange={this.handleChange}
+              handleAdd={this.handleAdd}
+              />
+
               <TodoList />
             </div>
         )
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -6,11 +6,12 @@ export default props => (
     <div role="form" className="todoForm">
         <Grid cols="12 9 10">
           
-        <input id="description" className="form-control" placeholder="Adicione uma tarefa" ></input>
+        <input id="description" className="form-control" placeholder="Adicione uma tarefa" onChange={props.handleChange} value={props.description}></input>
         </Grid>
         
         <Grid cols="12 3 2">
-        <IconButon style='primary' icon="plus" onClick={props.handleAdd} />
+        <IconButon style='primary' icon="plus" 
+        onClick={props.handleAdd} />
         </Grid>
     </div>
 
