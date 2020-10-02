a86e1cbe1829f885245f06fa5cd37cb189a31460
thayron <thayronf3@gmail.com>
2020-10-02 04:21:45 -0300

    Foi criado a funcionalidade usando rotas

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,16 +3,14 @@
  import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react';
-import Todo from '../todo/todo'
-import About from '../about/about'
-import Menu from '../template/menu'
+import Menu from '../template/menu';
+import Routes from './routes'
 
 export default props => (
      <div className="container">
      
         <Menu />
-        <Todo />
-        <About/>
+        <Routes />
     </div>
 )
    
