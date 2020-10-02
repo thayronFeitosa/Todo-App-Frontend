191b80d378f4b7a9489f9d416ec1ea25dac52825
thayronFeitosa <thayronf3@gmail.com>
2020-10-02 11:50:57 -0300

    Foi criado os componentes form e list

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import PageHeader from '../template/pageHeader'
+import todoForm from './todoForm';
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+
+
+
+
+
+
 export default class Todo  extends Component{
 
     render(){
         return(
             <div>
               <PageHeader name="Tarefas" small="Cadastro" />
+              <TodoForm />
+              <TodoList />
             </div>
         )
     }
