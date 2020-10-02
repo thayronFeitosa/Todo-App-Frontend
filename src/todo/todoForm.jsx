df91b66f4d767808f55bf4d07149d358e2ad064c
thayronFeitosa <thayronf3@gmail.com>
2020-10-02 12:47:29 -0300

    Foi criado o campo para inserir uma nova tarefa usando o bootstrap

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,14 @@
-import React from  'react';
+import React from 'react';
 
-export default props =>(
-        <div>
-            <h1>Form</h1>
+export default props => (
+    <div role="form" className="todoForm">
+        <div className="col-xs-12 col-sm-9 col-md-10">
+            <input id="description" className="form-control" placeholder="Adicione uma tarefa" ></input>
+
         </div>
+        <div className="col-xs-12 col-sm-3 col-md-2">
+            <button className="btn btn-primary"> <i className="fa fa-plus"></i> </button>
+        </div>
+    </div>
 
 )
