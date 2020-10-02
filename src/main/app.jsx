257a894dd5c4884298fc9cd35ce1111d5f674c07
thayron <thayronf3@gmail.com>
2020-10-02 03:50:08 -0300

    Criando as configurações baiscas

---
+++ src/main/app.jsx
@@ -0,0 +1,13 @@
+
+ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+ import '../../node_modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react';
+
+export default props => (
+     <div className="container">
+        <div>Teste</div>
+
+    </div>
+)
+   
